570379cbcbe64b17843d7e9d941886e878c12d94
Adam Seidler <adamseidlerr.as@gmail.com>
2021-11-24 21:52:03 +0100

    edit my program wit useing class

--- portfolio.py
+++ portfolio.py
@@ -1,149 +1,151 @@
-#!/usr/bin/env python3
 import datetime
 import requests
 
-DATE, COLUMN_NAME, TIME_STAMP, AMOUNT, CZK_PRICE = range(5)
+class SortedList:
 
+    def __init__(self, sequence = None):           
+        self.__key = str.lower
+        if sequence is None: 
+            self.__list = []
+        else:
+            self.__list = sorted(sequence, key = self.__key)
 
-def Get_Value(obj):
-    try:
-        obj = int(obj)
-    except ValueError:
-        pass
-    if isinstance(obj, str):
-        if obj.lower() == obj:
-            return 1/ord(obj)
-        return 1/(ord(obj.lower())-0.5)
-    return obj
-#print(Get_Value("4"))
+    def __str__(self):
+        """Vrátí řetězcovou verzi seznamu čitelnou pro člověka."""
+        return str(self.__list)
 
+    def add_new(self, value):
+        """přidá prvek do seznamu, který tam ještě není"""
+        if self.__list.count(value) == 0:
+            self.__list.append(value)
+            self.__list = sorted(self.__list, key = self.__key)
 
-def IsBigger(str_1, str_2): # porovná 2 řetězce a podle abecedy rozhodne který je "větší"
-    i = 0
-    while i < len(str_1):
-        try:
-            #print(Get_Value(str_1[i]),Get_Value(str_2[i]))
-            if Get_Value(str_1[i]) > Get_Value(str_2[i]):
-                return True
-            if Get_Value(str_1[i]) < Get_Value(str_2[i]):
-                return False
-        except IndexError: # pokud str_2 je podřetězcem str_1 -> str_1 je větší
-            return True
-        i += 1
-    return False # v případě, že jsou řetězce stejné
-#print(IsBigger('ASOB','TREZOR'))
+    def __getitem__(self, index):
+        """Vrátí hodnotu na zadané indexové pozici
+        >>> L[15]
+        """
+        return self.__list[index]
 
-
-def SortMinToMax(Data):
-    #list_of_date = []
-    #list_of_column = []
-    i = 0
-    while i < len(Data):
-        min = i
-        j = i + 1    
-        while j < len(Data): 
-            if IsBigger(Data[min][DATE], Data[j][DATE]):
-                min = j
-            if Data[min][DATE] == Data[j][DATE] and IsBigger(Data[j][COLUMN_NAME], Data[min][COLUMN_NAME]):
-                min = j
-            if Data[min][DATE] == Data[j][DATE] and Data[j][COLUMN_NAME] == Data[min][COLUMN_NAME] and IsBigger(Data[j][TIME_STAMP], Data[min][TIME_STAMP]):
-                min = j
-            j += 1
-        L_min = Data[min]
-        Data[min] = Data[i]
-        Data[i] = L_min
-        i += 1
-    #print(Data)
-    #print(list_of_column,list_of_date)
-    return Data
-
-def GenerateTab(SortedData):
-    list_of_date = []
-    list_of_column = []
-    for h in range(len(SortedData)):
-        if list_of_date.count(SortedData[h][DATE]) == 0: list_of_date.append(SortedData[h][DATE])
-        if list_of_column.count(SortedData[h][COLUMN_NAME]) == 0: list_of_column.append(SortedData[h][COLUMN_NAME])
-    list_of_column = sorted(list_of_column)
-    list_of_date = sorted(list_of_date)
-
-
-    tab = ["date"]
-    for column in list_of_column:
-        tab.append(column)
-    tab.append("Total[CZK]")
-    Tab = [tab]
-    for date in list_of_date:
-        line = [date]
-        total = 0
-        for column in list_of_column:
-            amount, in_czk  = Find_in_line(SortedData, date, column)
-            line.append(amount)
-            total += float(in_czk)
-        line.append(total)
-        Tab.append(line)
-
-
-    #print(Tab)
-    return Tab
-
-
-def Find_in_line(SortedData, line_name, column_name):
-    for line in SortedData:
-        if column_name in line and line_name in line:
-            return line[3], line[4]
-    return "-", 0
-
-
+    def __len__(self):
+        return len(self.__list)
 
     
-def PrintTab(Tab):
-    width = 20
-    for r in Tab:
-        for x in r:
-            print("{0:<{w}}".format(x,w=width),end="")
-        print("")
-        if r[0] == "date":
-            for x in r:
-                print("{0:-<{w}}".format("",w=width),end="")
-            print("")
+class Tab:
+
+    def __init__(self):
+        self.X = SortedList()
+        self.Y = SortedList()
+        self.Z = SortedList()
+        self.dict = {}
+
+    def __str__(self):
+        space = 20
+        r = "{0:<{s}}".format("date",s=space)
+        for y in self.Y:
+            r += "{0:<{s}}".format(y,s=space)
+        r += "{0:<{s}}".format("Total[CZK]",s=space)
+        r += "\n"
+        for x in self.X:
+            r += "{0:<{s}}".format(str(x),s=space)
+            tot = 0
+            for y in self.Y:
+                try:
+                    v = SortedList( self.dict[x][y].keys() )
+                    z = v[len(v)-1]
+                    #print(z)
+                    val = (self.dict[x][y][z][0])
+                    r += "{0:<{s}}".format(val,s=space)
+                    tot += float(self.dict[x][y][z][1])    
+                except:
+                    r += "{0:<{s}}".format("",s=space)
+            r += "{0:<{s}}\n".format(str(tot),s=space)
+        return r
+
+    def get_x(self):
+        return str(self.X)
+
+    def get_y(self):
+        return self.Y
+
+    def get_last(self, y):
+        x = self.X[len(self.X)-1]
+        v = SortedList( self.dict[x][y].keys() )
+        z = v[len(v)-1]
+        val = (self.dict[x][y][z][0])
+        return val
         
 
+    def update(self, L):
+        x = L[0]
+        y = L[1]
+        z = L[2]
+        value = L[3]
+        czk = L[4]
+        self.X.add_new(x)
+        self.Y.add_new(y)
+        self.Z.add_new(z)
+        try:
+            try:
+                self.dict[x][y].update({z:[value,czk]})
+            except:
+                self.dict[x].update({y:{z:[value,czk]}})
+        except:
+            self.dict.update({x:{y:{z:[value,czk]}}})
 
-           
-def GetData(Tab):
+   
+    
 
-    last_line = Tab[len(Tab)-1]
+
+
+
+
+
+def to_sats(a, m,url='https://blockchain.info/ticker'):
+    try:
+        eur = requests.get(url).json()['EUR']['15m']
+        czk = requests.get(url).json()['CZK']['15m']
+    except:
+        return None
+    if m == "CZK":
+        return a/czk*100000000, a
+    if m == "EUR":
+        return a/eur*100000000, a/eur*czk
+    if m == "BTC":
+        return a*100000000, a*czk
+
+
+def Get_data(T):
     t = str(datetime.datetime.now())[:str(datetime.datetime.now()).find(" ")]
     date = input("Zadejte datum [{0}]:".format(t))
     if not date:
         date = t
+    for n in T.get_y():
+        sat, czk = 0, 0
 
-    for i in range(len(Tab[0])):
-        if Tab[0][i] != "date" and Tab[0][i] != "Total[CZK]":
-            amount = input("Zadejte sumu pro {0}:".format(Tab[0][i]))
-            n = Tab[0][i]
-            if amount:
-                try:
-                    sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
-                except:
-                    sat, czk = 0, 0
-            if not amount:
-                try:
-                    amount = last_line[i]
-                    sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
-                except:
-                    amount = "-"
-                    czk = 0#"""
-        
+        amount = input( "Zadejte sumu pro {0}:".format(n) )
+        if amount:
+            try:
+                sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
+            except:
+                pass
+        if not amount:
+            try:
+                amount = T.get_last(n)
+                sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
+            except:
+                amount = "-"
+                czk = 0#"""
 
 
-            time_stamp = str(datetime.datetime.now()).replace(":","-").replace(" ","_")
-            f = open(filename, "a", encoding="utf8")
-            f.write("{0}:{1}:{2}:{3}:{4}\n".format(date,n,time_stamp,amount,czk))            
-            f.close()
 
+        time_stamp = str(datetime.datetime.now()).replace(":","-").replace(" ","_")
+        #print("{0}:{1}:{2}:{3}:{4}\n".format(date,n,time_stamp,amount,czk))
+        f = open(filename, "a", encoding="utf8")
+        f.write("{0}:{1}:{2}:{3}:{4}\n".format(date,n,time_stamp,amount,czk))            
+        f.close()
 
     while True:
+        sat, czk = 0, 0
         n = input("Zadejte novou položku (př.: Coinbase[BTC]):")
         if not n:
             break
@@ -152,66 +154,52 @@ def GetData(Tab):
             try:
                 sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
             except:
-                sat, czk = 0, 0
-        if not amount:
-            try:
-                amount = last_line[i]
-                sat, czk = to_sats(float(amount),n[n.find("[")+1:n.find("]")])
-            except:
-                amount = "-"
-                czk = 0#"""
+                pass
+    
         time_stamp = str(datetime.datetime.now()).replace(":","-").replace(" ","_")
+        #print("{0}:{1}:{2}:{3}:{4}\n".format(date,n,time_stamp,amount,czk))
         f = open(filename, "a", encoding="utf8")
         f.write("{0}:{1}:{2}:{3}:{4}\n".format(date,n,time_stamp,amount,czk))            
         f.close()
 
 
 
-def to_sats(a, m,url='https://blockchain.info/ticker'):
-    eur = requests.get(url).json()['EUR']['15m']
-    czk = requests.get(url).json()['CZK']['15m']
-    if m == "CZK":
-        return a/czk*100000000, a
-    if m == "EUR":
-        return a/eur*100000000, a/eur*czk
-
-    if m == "BTC":
-        return a*100000000, a*czk
-    
 
 
 
-
-        
-        
-    
-
-    
-            
-        
-
-
-
- 
-#SortMinToMax(Data)
 
 
 while True:
-
+    T = Tab()
+    
+    #T.update(["2021-11-02","BTC","2021-11-09_14-53-25.753076","A.32158","400000"])
+    #print(T)
     filename='portfolio_data.txt'
-    Data = list()
     for line in open(filename, encoding="utf8"):    # otevření souboru a načtení řádky po řádku
         line = line.rstrip()                        # rstrip() - odstraní bílá místa na pravo
         field = line.split(":") # type of fields = list
-        Data.append(field)
+        T.update(field)
+    print(T)
+    
+    Get_data(T)
 
-    SortedData = SortMinToMax(Data)
-    MyTab = GenerateTab(SortedData)
-    PrintTab(MyTab)
-    GetData(MyTab)
 
-#print(MyTab[len(MyTab)-1])
-#"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
